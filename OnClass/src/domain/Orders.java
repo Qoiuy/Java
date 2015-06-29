7fb7c791b90d600948f08eab3f82977f6ef0c910
qoiuy <1010255393@qq.com>
2015-06-29 23:33:08 +0800

    实体类domain中Orders.java类中的修改(简)price变量改为float，num变量改为int ，status变量改为status,添加Customer c变量 ，添加 List<Ordersitem> item变量,重新写G T 有参 无参

--- OnClass/src/domain/Orders.java
+++ OnClass/src/domain/Orders.java
@@ -1,25 +1,30 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Orders {
+	
 	public Orders() {
 		super();
 		// TODO 自动生成的构造函数存根
 	}
-	public Orders(String id, String ordernum, String num, String price,
-			String status, String customerid) {
+	public Orders(String id, String ordernum, int num, float price, int status,
+			Customer c, List<Ordersitem> items) {
 		super();
 		this.id = id;
 		this.ordernum = ordernum;
 		this.num = num;
 		this.price = price;
 		this.status = status;
-		this.customerid = customerid;
+		this.c = c;
+		this.items = items;
 	}
 	@Override
 	public String toString() {
 		return "Orders [id=" + id + ", ordernum=" + ordernum + ", num=" + num
-				+ ", price=" + price + ", status=" + status + ", customerid="
-				+ customerid + "]";
+				+ ", price=" + price + ", status=" + status + ", c=" + c
+				+ ", items=" + items + "]";
 	}
 	public String getId() {
 		return id;
@@ -33,35 +38,42 @@ public class Orders {
 	public void setOrdernum(String ordernum) {
 		this.ordernum = ordernum;
 	}
-	public String getNum() {
+	public int getNum() {
 		return num;
 	}
-	public void setNum(String num) {
+	public void setNum(int num) {
 		this.num = num;
 	}
-	public String getPrice() {
+	public float getPrice() {
 		return price;
 	}
-	public void setPrice(String price) {
+	public void setPrice(float price) {
 		this.price = price;
 	}
-	public String getStatus() {
+	public int getStatus() {
 		return status;
 	}
-	public void setStatus(String status) {
+	public void setStatus(int status) {
 		this.status = status;
 	}
-	public String getCustomerid() {
-		return customerid;
+	public Customer getC() {
+		return c;
 	}
-	public void setCustomerid(String customerid) {
-		this.customerid = customerid;
+	public void setC(Customer c) {
+		this.c = c;
+	}
+	public List<Ordersitem> getItems() {
+		return items;
+	}
+	public void setItems(List<Ordersitem> items) {
+		this.items = items;
 	}
 	private String id;
 	private String ordernum;
-	private String num;
-	private String price;
-	private String status;
-	private String customerid;
+	private int num;
+	private float price;
+	private int status;
+	private Customer c;//这个代表一个订单下可以有多个订单项
+	private List<Ordersitem> items = new ArrayList<Ordersitem>();
 	
 }
