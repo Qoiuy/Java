c566e2a62dac26e503df2836df3aa4bafc03291a
qoiuy <1010255393@qq.com>
2015-06-30 11:09:41 +0800

    customerSrviceimpl 名字改为 customerServiceimpl

---
+++ OnClass/src/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,16 @@
+package service.impl;
+
+import dao.CustomerDao;
+import dao.impl.CustomerDaoImpl;
+import domain.Customer;
+import service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService{
+
+	//业务逻辑层依赖于数据访问层
+	private CustomerDao customerDao = new CustomerDaoImpl();
+	public Customer login(String username, String password) {
+		return customerDao.findCustomerByUsernameAndPwd(username, password);
+	}
+
+}
