741510afc9b4a1485f27f5e7f16627db1f0c0317
qoiuy <1010255393@qq.com>
2015-06-30 09:36:34 +0800

    提取完Dao接口之后 修改CategoryDao

--- OnClass/src/dao/CategoryDao.java
+++ OnClass/src/dao/CategoryDao.java
@@ -11,34 +11,6 @@ import domain.*;
  * @author root
  *
  */
-public interface CategoryDao {
+public interface CategoryDao extends Dao<Category> {
 
-	/**
-	 * 用于将一个Category对象保存到category表中
-	 * @param category
-	 */
-	public void save(Category category);
-	/**
-	 * 更新
-	 * @param category
-	 */
-	public void update(Category category);
-	/**
-	 * 删除制定编号的记录
-	 * @param id
-	 */
-	public void delete(Serializable id);
-	
-	/**
-	 * 根据ID,获取一个分类信息
-	 * @param id
-	 * @return
-	 */
-	public Category findOne(Serializable id);
-	
-	/**
-	 * 获取所有分类列表
-	 * @return
-	 */
-	public List<Category> findAll();
 }
